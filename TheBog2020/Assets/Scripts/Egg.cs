490f18f5cc74e274c804e79d000018d25b07ede3
cgbNYU <cgb316@nyu.edu>
2020-02-09 13:55:14 -0500

    created egg script and set up spawning basics for the nest

---
+++ TheBog2020/Assets/Scripts/Egg.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Tracks team ID
+/// Track if it is in the Nest
+/// Tracks whether it is held
+/// </summary>
+public class Egg : MonoBehaviour
+{
+    //0 = neutral, 1 = red, 2 = blue
+    public int TeamID;
+    
+    //Bools
+    public bool IsHeld;
+    public bool InNest;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- TheBog2020/Assets/Scripts/Nest.cs
+++ TheBog2020/Assets/Scripts/Nest.cs
@@ -9,6 +9,13 @@ using UnityEngine;
 /// </summary>
 public class Nest : MonoBehaviour
 {
+    //0 = neutral, 1 = red, 2 = blue
+    public int TeamID;
+    
+    //Egg list
+    public List<Transform> SpawnLocations; //where the eggs spawn on the nest. Set in inspector
+    public List<Egg> EggList = new List<Egg>(); //holds reference to all of the eggs for this team
+    
     // Start is called before the first frame update
     void Start()
     {
@@ -19,5 +26,20 @@ public class Nest : MonoBehaviour
     void Update()
     {
         
+    }
+    
+    //Go through the spawn locations and add an egg to each
+    public void SpawnEggs()
+    {
+        foreach (var spawn in SpawnLocations)
+        {
+            GameObject newEgg = Instantiate(Resources.Load<GameObject>("Prefabs/Egg"));
+            newEgg.transform.position = spawn.position;
+            Egg eggScript = newEgg.GetComponent<Egg>();
+            eggScript.TeamID = TeamID;
+            eggScript.IsHeld = false;
+            eggScript.InNest = true;
+            EggList.Add(eggScript);
+        }
     }
 }
