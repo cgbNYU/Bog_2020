c7d81a3f874be0d3a226d718e849df064c8bb8b0
cgbNYU <cgb316@nyu.edu>
2020-02-09 13:37:20 -0500

    created nest script

---
+++ TheBog2020/Assets/Scripts/Nest.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Spawn and track eggs for one team
+/// Respawn players on death
+/// Alert game to a loss by eggs
+/// </summary>
+public class Nest : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
