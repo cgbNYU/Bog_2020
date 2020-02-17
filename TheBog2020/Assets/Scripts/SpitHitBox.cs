89532222ea1eb4dbaa2306976ba4391aba0441c4
cgbNYU <cgb316@nyu.edu>
2020-02-17 15:17:39 -0500

    spitting basics implemented. Goes forward, knocks back players

---
+++ TheBog2020/Assets/Scripts/SpitHitBox.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpitHitBox : MonoBehaviour
+{
+    public float KnockBackForce;
+    public int TeamID;
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            PlayerController otherPlayer = other.GetComponent<PlayerController>();
+            if (otherPlayer.TeamID != TeamID)
+            {
+                Vector3 knockBackDir = transform.position - other.transform.position;
+                other.GetComponent<Rigidbody>().AddForce(knockBackDir * KnockBackForce);
+                Destroy(gameObject);
+            }
+        }
+    }
+}
