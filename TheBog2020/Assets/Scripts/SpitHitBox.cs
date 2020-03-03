9c93c3874a94c68be77f684f0843b6212cba0795
cgbNYU <cgb316@nyu.edu>
2020-03-03 16:27:16 -0500

    spit kills

--- TheBog2020/Assets/Scripts/SpitHitBox.cs
+++ TheBog2020/Assets/Scripts/SpitHitBox.cs
@@ -13,12 +13,22 @@ public class SpitHitBox : MonoBehaviour
             PlayerController otherPlayer = other.GetComponent<PlayerController>();
             if (otherPlayer.TeamID != TeamID)
             {
-                Vector3 knockBackDir = transform.position - other.transform.position;
-                knockBackDir = new Vector3(knockBackDir.x, 0, knockBackDir.z);
-                knockBackDir.Normalize();
-                other.GetComponent<Rigidbody>().AddForce(knockBackDir * KnockBackForce);
+                KillPlayer(otherPlayer);
                 Destroy(gameObject);
             }
         }
+    }
+
+    private void KillPlayer(PlayerController pc)
+    {
+        pc.KillPlayer();
+    }
+    
+    private void KnockBackPlayer(Collider other)
+    {
+        Vector3 knockBackDir = transform.position - other.transform.position;
+        knockBackDir = new Vector3(knockBackDir.x, 0, knockBackDir.z);
+        knockBackDir.Normalize();
+        other.GetComponent<Rigidbody>().AddForce(knockBackDir * KnockBackForce);
     }
 }
