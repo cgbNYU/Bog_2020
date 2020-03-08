bceb3111b22b4764583a0838a7ea78fedde9e058
Siddarth <siddarthrgovindan@gmail.com>
2020-03-07 19:44:27 -0500

    Created a new scriptable object to store different sets of player tuning variables

---
+++ TheBog2020/Assets/Scripts/PlayerControllerTuning.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// A scriptable object that lets us create and store different sets of player controller tuning variables.
+/// Create a new tuning from the Inspector by right-clicking Create > Player Controller Tuning .
+/// </summary>
+
+[CreateAssetMenu(fileName = "New PC Tuning",menuName = "Player Controller Tuning")]
+public class PlayerControllerTuning : ScriptableObject
+{
+    [Header("Movement Variables")] 
+    public float MaxForce;
+    public float WingOffset;
+    public float WingDrag; //MUST BE NEGATIVE
+    public float QuadAngularDrag;
+    public int BufferFrames;
+    public AnimationCurve WingForceCurve = new AnimationCurve();
+    
+    [Header("Attack Variables")]
+    public float LungeTargetRadius;
+    public float LungeRange;
+    public float LungeForce;
+    public float LungeTime;
+    public float ClashForce;
+
+    public float SpitForce;
+    public float SpitTime;
+    
+    [Header("Lock On Variables")] 
+    public float LockOnRange;
+    [Range(0.01f, 5f)]
+    public float LockTorque;
+    [Range(-0.001f, -2f)]
+    public float LockDrag;
+    
+}
