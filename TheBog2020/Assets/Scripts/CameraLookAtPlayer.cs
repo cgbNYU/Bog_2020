b2355634024ead079c037d46ede27f74eccb1f12
meisteronly <chapinbr@gmail.com>
2020-04-20 19:13:02 -0400

    Installed cinemachine

---
+++ TheBog2020/Assets/Scripts/CameraLookAtPlayer.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraLookAtPlayer : MonoBehaviour
+{
+    
+    //Public Tuneables
+    public Transform TargetPlayer;
+    public float RotateSpeed;
+    
+    //Private Variables
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Quaternion newRotation =
+            Quaternion.LookRotation(TargetPlayer.position - transform.position, Vector3.up);
+
+        newRotation = Quaternion.Euler(new Vector3(transform.localRotation.x, newRotation.y * Mathf.Rad2Deg, transform.localRotation.z));
+        
+        transform.localRotation = newRotation;
+    }
+}
---
+++ TheBog2020/Assets/Scripts/PlayerHover.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Raycast down from a certain number of points to find distance to ground
+/// Increase force up based on how close you are to the ground
+/// </summary>
+public class PlayerHover : MonoBehaviour
+{
+    
+    //Public Tuneables
+    public float HoverForce;
+    
+    //Private
+    private Rigidbody _rb;
+
+    private void Start()
+    {
+        _rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Begin raycast above player
+        Vector3 rayCastOrigin = new Vector3(transform.position.x, transform.position.y + 1, transform.position.z);
+        RaycastHit hit;
+        if (Physics.Raycast(transform.position, Vector3.down, out hit))
+        {
+            
+            //Grab the distance to the ground
+            float groundDist = Vector3.Distance(hit.point, transform.position);
+            Debug.Log("Ground dist = " + groundDist);
+            //Apply an upward force divided by distance to ground
+            _rb.AddForce(Vector3.up * HoverForce * (1 / groundDist));
+        }
+    }
+}
