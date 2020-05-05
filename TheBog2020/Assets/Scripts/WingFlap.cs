a9d1b778e81538a4d519aea3de6a79d81ace1ae4
meisteronly <chapinbr@gmail.com>
2020-05-05 12:44:13 -0400

    Added AMbianceAudioSource to LevelDesign and changed the tuning in GM

--- TheBog2020/Assets/Scripts/WingFlap.cs
+++ TheBog2020/Assets/Scripts/WingFlap.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using AlmenaraGames;
 using UnityEngine;
 
 /// <summary>
@@ -15,6 +16,7 @@ public class WingFlap : MonoBehaviour
     //Tunings
     public float MinAmp;
     public float MaxAmp;
+    public float FlapSoundTime;
     
     //Bool to prevent changing all the time
     private bool _hasInputLeft;
@@ -22,6 +24,10 @@ public class WingFlap : MonoBehaviour
     
     //PlayerController ref
     private PlayerController _pc;
+    
+    //Sounds
+    private MultiAudioSource _flapSoundSource;
+    private float _soundTimer;
     
     // Start is called before the first frame update
     void Start()
@@ -40,10 +46,19 @@ public class WingFlap : MonoBehaviour
         {
             mr.material.SetFloat("_Amplitude", MinAmp);
         }
+
+        _soundTimer = FlapSoundTime;
+
+        _flapSoundSource = GetComponent<MultiAudioSource>();
     }
 
     // Update is called once per frame
     void Update()
+    {
+        InputResponse();
+    }
+
+    void InputResponse()
     {
         //If the player has not pushed the left stick and pushes it, set the wings to max
         if (_pc.ReadLeftInput().magnitude > 0 && !_hasInputLeft)
@@ -82,6 +97,19 @@ public class WingFlap : MonoBehaviour
             {
                 mr.material.SetFloat("_Amplitude", MinAmp);
             }
+        }
+        
+        SoundOnFlaps();
+    }
+
+    void SoundOnFlaps()
+    {
+        _soundTimer -= Time.deltaTime;
+
+        if (_soundTimer <= 0)
+        {
+            _flapSoundSource.Play();
+            _soundTimer = FlapSoundTime;
         }
     }
 }
