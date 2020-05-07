197155c5c395901199ea072cbb07fd4e2394c91e
Siddarth <siddarthrgovindan@gmail.com>
2020-05-07 14:38:35 -0400

    created a reference to the spit sacs in playermodelindex and linked it up in the player models prefab

--- TheBog2020/Assets/Scripts/PlayerModelIndex.cs
+++ TheBog2020/Assets/Scripts/PlayerModelIndex.cs
@@ -4,12 +4,21 @@ using UnityEngine;
 
 public class PlayerModelIndex : MonoBehaviour
 {
+   
    public Transform bugTailRef;
    public Transform[] playerModelsByPlayerCam;
    
+   [Header("Player meshes by camera model")]
    //These are the actual mesh renderer lists
    public List<MeshRenderer> Player1CamMeshRenderers = new List<MeshRenderer>();
    public List<MeshRenderer> Player2CamMeshRenderers = new List<MeshRenderer>();
    public List<MeshRenderer> Player3CamMeshRenderers = new List<MeshRenderer>();
    public List<MeshRenderer> Player4CamMeshRenderers = new List<MeshRenderer>();
+
+   [Header("Spit Sacs")] 
+   public List<MeshRenderer> spitSacs1;
+   public List<MeshRenderer> spitSacs2;
+   public List<MeshRenderer> spitSacs3;
+   public List<MeshRenderer> spitSacs4;
+   public List<MeshRenderer> spitSacs5;
 }
