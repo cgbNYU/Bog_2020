0810750874b850db2ad4030f2bbebb42b0b09bb5
meisteronly <chapinbr@gmail.com>
2020-05-10 20:08:32 -0400

    Spit has a trail renderer and a particle explosion when it hits

--- TheBog2020/Assets/Scripts/SpitHitBox.cs
+++ TheBog2020/Assets/Scripts/SpitHitBox.cs
@@ -7,7 +7,16 @@ public class SpitHitBox : MonoBehaviour
 {
     public float spitLifeTime;
     public float KnockBackForce;
+    public Collider Col;
+    public MeshRenderer MR;
+    public ParticleSystem Particles;
     public int TeamID;
+
+    IEnumerator destroySelfAfterLifeTime(float t)
+    {
+        yield return new WaitForSeconds(t);
+        Destroy(gameObject);
+    }
 
     private void OnCollisionEnter(Collision other)
     {
@@ -28,20 +37,28 @@ public class SpitHitBox : MonoBehaviour
             if (otherPlayer.TeamID != TeamID)
             {
                 KillPlayer(otherPlayer);
-                Destroy(gameObject);
+                DeactivateSpit();
             }
         }
         // Don't destroy the spit when it collides with these objects 
         if (!other.gameObject.CompareTag("Nest") && !other.gameObject.CompareTag("DropTrigger") &&
             !other.gameObject.CompareTag("Spit") && !other.gameObject.CompareTag("Egg"))
         {
-            Destroy(gameObject);
+            DeactivateSpit();
         }
     }
 
     private void KillPlayer(PlayerController pc)
     {
         pc.KillPlayer();
+    }
+
+    private void DeactivateSpit()
+    {
+        Col.enabled = false;
+        MR.enabled = false;
+        Particles.Play();
+        StartCoroutine(destroySelfAfterLifeTime(spitLifeTime));
     }
     
     private void KnockBackPlayer(Collider other)
